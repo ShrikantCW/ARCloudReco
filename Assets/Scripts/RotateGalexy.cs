48e5a81db19be5cd3df847a0674509a7fd0907ac
shrikant <shrikant.t@codewalla.com>
2020-03-02 11:32:40 +0530

    Streaming assets vuforia files added

---
+++ Assets/Scripts/RotateGalexy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGalexy : MonoBehaviour
+{
+    [SerializeField]
+    float rotateSpeed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, rotateSpeed, 0f));
+    }
+}
